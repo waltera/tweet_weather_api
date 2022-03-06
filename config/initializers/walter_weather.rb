70dd5da199b8886f52b41036cf9818d32693f84b
Walter Longo Neto <walterlongoneto@gmail.com>
2022-03-06 16:27:20 -0300

    Change env variable to better represent

--- config/initializers/walter_weather.rb
+++ config/initializers/walter_weather.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 WalterWeather::Config::Service.configure do |config|
-  config.app_key = ENV["WALTER_WEATHER_API_KEY"]
+  config.app_key = ENV["OPEN_WEATHER_API_KEY"]
 end
